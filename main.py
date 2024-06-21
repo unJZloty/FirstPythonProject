b84ae4905853d87f8ee39bd5ab86274b37329d9f
unJZloty <zolotenkiy@gmail.com>
2024-06-21 12:32:27 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+a = "Hello, world"
+print (len (a))
+
+first = 3
+second = 5
+summa = first + second
+diff = first - second
+print (summa, diff)
+
+first = 3
+second = 5
+third = 7
+mean = (first + second + third) / 2
+print (mean)
+
+first_string = "Вторник"
+second_string = "Понедельник"
+print (second_string + ", " + first_string)
+
+a = 3
+b = 5
+c = 7
+f = ((a*b) + (a*c)) ** 3 / 2
+print (f)
+
+
+print('Hi, PyCharm')
+x = 43
+y = 32
+print(x * y)
+print("End line")

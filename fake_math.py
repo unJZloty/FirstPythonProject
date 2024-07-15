47ed69c878191bba2d0d5231d0aa1accd7e01d19
unJZloty <zolotenkiy@gmail.com>
2024-07-15 19:44:34 +0300

    Add files via upload

--- fake_math.py
+++ fake_math.py
@@ -1,5 +1,5 @@
 def divide(first, second):
     if second != 0:
-        print(first / second)
+        return (first / second)
     if second == 0:
-        print('Ошибка')
+        return ('Ошибка')
--- true_math.py
+++ true_math.py
@@ -1,6 +1,6 @@
 from math import inf
 def divide(first, second):
     if second != 0:
-        print(first / second)
+        return (first / second)
     if second == 0:
-        print(inf)
+        return (inf)
